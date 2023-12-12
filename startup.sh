20bf2f8bf4b304db8a39ef099715dabbfb136396
Janindu Mapa <janindumapa100@gmail.com>
2023-12-12 18:07:34 +0530

    small change

--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install dependencies in the client folder
-cd usr/src/app/client
+cd /usr/src/app/client
 npm install
 
 # Install dependencies in the server folder
-cd usr/src/app/server
+cd /usr/src/app/server
 npm install
 
 # Go back to the root directory where docker-compose.yml is located
-cd usr/src/app/
+cd /usr/src/app/
 
 # Run docker-compose up
 docker-compose up
