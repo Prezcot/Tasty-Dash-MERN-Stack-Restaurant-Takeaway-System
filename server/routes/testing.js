797b18eb8cfdd8d9b4ec4d19c3fe7a18a8efcf3a
Janindu Mapa <janindumapa100@gmail.com>
2023-12-14 23:51:57 +0530

    Added mongoose connection and successfully passed data - also changed the connection in the .env with the proper database

--- server/routes/testing.js
+++ server/routes/testing.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+
+const item = mongoose.model("item", { name: String });
 
 router.get("/", (req, res) => {
   res.send("Permission Denied");
@@ -6,7 +9,10 @@ router.get("/", (req, res) => {
 
 router.post("/data/send", async (req, res) => {
   let data = req.body;
-  console.log(data);
+  saveName = new item({ name: data.name });
+
+  confirm = await saveName.save();
+  console.log("Item Saved:", confirm);
 });
 
 module.exports = router;
