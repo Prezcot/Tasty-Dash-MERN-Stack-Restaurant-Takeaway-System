e00d703e25d4b8e7d7a56c53aeff3e48f2d23d34
C T W Mudiyanselage <114335319+Charith0401@users.noreply.github.com>
2023-12-18 17:32:26 +0530

    added a nav bar for users

--- client/src/components/User/Dashboard.jsx
+++ client/src/components/User/Dashboard.jsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
+import UserNavBar from "./UserNavBar";
+
 function Dashboard()
 {
+
+    useEffect(()=>{
+
+
+    })
     return(
         <div>
+            <UserNavBar></UserNavBar>
             Dashboard
         </div>
     )
