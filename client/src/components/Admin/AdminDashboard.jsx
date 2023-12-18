c47af101e409f9ec864cf3c2116a6de8ecbfe4e3
Janindu Mapa <janindumapa100@gmail.com>
2023-12-18 14:18:00 +0530

    fix

--- client/src/components/Admin/AdminDashboard.jsx
+++ client/src/components/Admin/AdminDashboard.jsx
@@ -1,7 +1,13 @@
+import AdminNavBar from "./AdminNavBar";
+
 function AdminDashboard() {
-  return(
-  <h1>Testing</h1>
-  )
+  return (
+    <>
+      <AdminNavBar />
+      <h1>Pending Orders</h1>
+      <ul className="list-group"></ul>
+    </>
+  );
 }
 
 export default AdminDashboard;
