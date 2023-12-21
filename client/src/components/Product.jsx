97109dfc8a415566d4c37aee5de1b4cb4441370c
Chamath D Udugampola <114849879+CDayanU@users.noreply.github.com>
2023-12-21 16:23:56 +0530

    Order Summary and Counter Logic FInalized

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -10,40 +10,42 @@ function Product ({itemProp,indexProp, cartProp, updateProp}){
     let [quantity,setQuantity] = useState(parseInt(qty));
 
     function handleQuantity(e){
-        if(e.target.name=="minus"){
+        if(e.target.name=="minus" && quantity > 0){
             updateProp();
             setQuantity(quantity=quantity-1);
             cartProp[indexProp] = name+","+price+","+(JSON.stringify(quantity));
-            localStorage.setItem("cart",JSON.stringify(cartProp));
+            sessionStorage.setItem("cart",JSON.stringify(cartProp));
             console.log("changedItem"+cartProp[indexProp]);
         };
         if(e.target.name=="plus"){
             updateProp();
             setQuantity(quantity=quantity+1);
             cartProp[indexProp] = name+","+price+","+(JSON.stringify(quantity));
-            localStorage.setItem("cart",JSON.stringify(cartProp));
+            sessionStorage.setItem("cart",JSON.stringify(cartProp));
             console.log("changedItem"+cartProp[indexProp]);
         };
     }
 
     // let thinalcart = ["Pizza,600,1","Pebbles,400,3","Lava Cake,200,2"];
     
-
-
-    return(
-        <div className="item">
-            <img src="Biryani.png" width="130px" height="106px"/>
-            <p>{name}</p>
-            <p>Rs.{price} each</p>
-            <div className="counter">
-                <img name="minus" src="Minus.png" className="counter-button" onClick={(e)=>handleQuantity(e)}/>
-                <div id="counter-number">
-                <p>{quantity}</p>
+    if (quantity > 0) {
+        return(
+            <div className="item">
+                <img src="Biryani.png" width="130px" height="106px"/>
+                <p>{name}</p>
+                <p>Rs.{price} each</p>
+                <div className="counter">
+                    <img name="minus" src="Minus.png" className="counter-button" onClick={(e)=>handleQuantity(e)}/>
+                    <div id="counter-number">
+                    <p>{quantity}</p>
+                    </div>
+                    <img name="plus" src="Plus.png" className="counter-button" onClick={(e)=>handleQuantity(e)}/>
                 </div>
-                <img name="plus" src="Plus.png" className="counter-button" onClick={(e)=>handleQuantity(e)}/>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return null;
+    }
 }
 
 export default Product;
--- client/src/components/SummaryItem.jsx
+++ client/src/components/SummaryItem.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useState } from "react";
 
 function SummaryItem ({itemProp2, indexProp2, cartProp2}){
 
     let [name2, price2, qty2] = itemProp2.split(",");
     let totalPrice = parseInt(price2)*parseInt(qty2);
+
+    if(qty2>0)
+    {
     return(
         <div className="details">
             <div id="indi-detail">
@@ -18,6 +20,11 @@ function SummaryItem ({itemProp2, indexProp2, cartProp2}){
             </div>
         </div>
     );
+    }
+
+    else{
+        return null;
+    }
 }
 
 export default SummaryItem;
