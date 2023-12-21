156c2f5013ed0ef69bc0714d504f7d1b32f41513
C T W Mudiyanselage <114335319+Charith0401@users.noreply.github.com>
2023-12-21 21:20:21 +0530

    moved all schemas and models to a single schemas file which will then be imported into the relevant routing files on the server

--- server/routes/AdminDashboardData.js
+++ server/routes/AdminDashboardData.js
@@ -1,13 +1,6 @@
 const { default: mongoose } = require("mongoose");
 const router = require("express").Router();
-const item=require("../Schemas/Schemas");
-// const item = mongoose.model("orders", {
-//   username: String,
-//   order_id: String,
-//   payment_id: String,
-//   email: String,
-//   items: Array,
-// });
+const {item,Menu,users}=require("../Schemas/Schemas");
 
 router.get("/", (req, res) => {
   res.send("Permission Denied");
