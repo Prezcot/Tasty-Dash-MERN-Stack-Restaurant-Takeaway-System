e5bac366d17f4102b2ae48c477502cdf29816d1c
Janindu Mapa <janindumapa100@gmail.com>
2023-12-21 23:29:54 +0530

    added socket.io functionality and fixed previous issues

--- server/Schemas/Schemas.js
+++ server/Schemas/Schemas.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const item = mongoose.model("orders", {
   username: String,
   order_id: String,
   payment_id: String,
   email: String,
   items: Array,
+  order_status: String,
 });
 
-module.exports=item;
+module.exports = item;
