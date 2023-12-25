a8fa4d68504dbe9c9d879ab2c8f3920f0a05b9ed
Janindu Mapa <janindumapa100@gmail.com>
2023-12-25 21:18:50 +0530

    added dashboard test

--- client/src/components/Tests/AdminDashboard.test.js
+++ client/src/components/Tests/AdminDashboard.test.js
@@ -1,9 +1,16 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import AdminNavBar from "../Admin/AdminNavBar";
+import AdminDashboard from "../Admin/AdminDashboard";
 
 test("whether AdminNavBar renders properly", () => {
   render(<AdminNavBar />);
   const title = screen.getByText(/admin dashboard/i);
+  expect(title).toBeInTheDocument();
+});
+
+test("whether AdminDashbaord renders properly", () => {
+  render(<AdminDashboard />);
+  const title = screen.getByText(/pending orders/i);
   expect(title).toBeInTheDocument();
 });
