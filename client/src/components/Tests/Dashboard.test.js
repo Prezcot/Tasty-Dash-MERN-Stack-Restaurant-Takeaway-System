f8560f17563801818f6d94802b749fc11e647997
Charith <114335319+Charith0401@users.noreply.github.com>
2023-12-26 09:42:27 +0530

    console errors are now silenced

--- client/src/components/Tests/Dashboard.test.js
+++ client/src/components/Tests/Dashboard.test.js
@@ -1,18 +1,19 @@
-// import React from "react";
-// import { render, fireEvent, waitFor,getAllByText,getByText } from "@testing-library/react";
-// import {handleSignIn} from "../User/SignIn";
-// import SignIn from "../User/SignIn";
-// import { BrowserRouter } from "react-router-dom";
-// import axios from "axios";
-// import '@testing-library/jest-dom';
-// jest.mock("axios");
-// // describe("UNIT TEST - DASHBOARD COMPONENT", () => {
-// //     it("Dashboard Component Is Successfully Rendering", () => {
-// //         var {getAllByText}=render(
-// //             <BrowserRouter>
-// //                 <Dashboard/>
-// //             </BrowserRouter>);
-// //         const element=getAllByText("Dashboard");
-// //     });
+import React from "react";
+import { render, fireEvent, waitFor,getAllByText,getByText } from "@testing-library/react";
+import {handleSignIn} from "../User/SignIn";
+import SignIn from "../User/SignIn";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import '@testing-library/jest-dom';
+jest.mock("axios");
+describe("UNIT TEST - DASHBOARD COMPONENT", () => {
+    it("Dashboard Component Is Successfully Rendering", () => {
+        var {getAllByText}=render(
+            <BrowserRouter>
+                <Dashboard/>
+            </BrowserRouter>);
+        const element=getAllByText("Dashboard");
+        expect(element.length).toBeGreaterThan(0);
+    });
 
-// // });
+});
