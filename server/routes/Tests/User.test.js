27cfeff4a88321ab43339d59453bbba0e2bab69e
Charith <114335319+Charith0401@users.noreply.github.com>
2023-12-28 13:58:38 +0530

    working on user route server side testing and solved an issue where bcrypt was taking too long to resolve a promise and so an alternative method of using a mock function to resolve a predefined promise was used

--- server/routes/Tests/User.test.js
+++ server/routes/Tests/User.test.js
@@ -21,7 +21,22 @@ app.use("/users", User);
 describe("UNIT TEST - User Route",()=>{
     it("Successfully Responds To Requests",async()=>{
         const username="user";
-        const res=await supertest(app).get(`/users/userinfo/:${username}`).expect('Content-Type', /json/).expect(200);
+        const res=await request(app).get(`/users/userinfo/${username}`);
+        expect(res.status).toBe(200);
         expect(res.body).toEqual(expect.any(Object));
     });
+});
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(() => Promise.resolve('hashedPassword'))
+}));
+describe("INTEGRATION TEST - User Route",()=>{
+  it("Successfully Adds New Users To Database",async()=>{
+    const res=await request(app).post("/users/signup",{username:"george",email:"george@gmail.com",phonenumber:"5438922345",password:"George123,"})
+    console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({message:"Successful Register"});
+    //const user=new users({username:"George",type:"User",email:"george@gmail.com",phonenumber:"5438922345",password:"George123,"});
+    //await user.save();
+
+  });
 });
--- server/routes/User.js
+++ server/routes/User.js
@@ -9,10 +9,15 @@ const {item,Menu,users}=require("../Schemas/Schemas");
 router.get("/userinfo/:username",async (req,res,next)=>
 {
     var username=req.params.username;
-    var query=await users.find({username:username}).catch((err)=>res.status(400).json({message:err}));
-    if (query.length>0)
-    {
-        res.status(200).json({username:query[0].username,email:query[0].email,phonenumber:query[0].phonenumber});
+    try{
+        var query=await users.find({username:username}).catch((err)=>res.status(400).json({message:err}));
+        if (query.length>0)
+        {
+            res.status(200).json({username:query[0].username,email:query[0].email,phonenumber:query[0].phonenumber});
+        }
+    }
+    catch(err){
+        res.status(400).json({message:err});
     }
 });
 
@@ -20,85 +25,105 @@ router.get("/userinfo/:username",async (req,res,next)=>
 router.post("/signin",async (req,res,next)=>{ //This route handler handles all signin requests
     const {username,email,phonenumber,password,cnfrmpassword}=req.body;
     var valid=false;
-    var query=await users.find({username:username}).catch((err)=>res.status(400).json({message:err}));
-    // var finddoc=await users.find({username:username,password:password}).catch((err)=>res.status(400).json({message:err}));
+    try{
+        var query=await users.find({username:username}).catch((err)=>res.status(400).json({message:err}));
+        // var finddoc=await users.find({username:username,password:password}).catch((err)=>res.status(400).json({message:err}));
 
-    if (query.length>0)
-    {
-        valid= await bcrypt.compare(password,query[0].password);
-    }
-    if (query.length>0 && valid)
-    {
-        if (query[0].type=="User")
+        if (query.length>0)
         {
-            res.status(200).json({message:"Account Registered",user:"User",email:query[0].email});
+            valid= await bcrypt.compare(password,query[0].password);
         }
-        else{
-            res.status(200).json({message:"Account Registered",user:"Admin"});
+        if (query.length>0 && valid)
+        {
+            if (query[0].type=="User")
+            {
+                res.status(200).json({message:"Account Registered",user:"User",email:query[0].email});
+            }
+            else{
+                res.status(200).json({message:"Account Registered",user:"Admin"});
+            }
         }
-    }
-    else
-    {
-        res.status(400).json({message:"Account Not Registered/Invalid Credentials"});
+        else
+        {
+            res.status(400).json({message:"Account Not Registered/Invalid Credentials"});
+        }
+    }catch(err){
+        res.status(400).json({message:err});
     }
 });
 
 router.post("/signup",async(req,res,next)=>{ //This route handler handles all signup requests
     var {username,email,phonenumber,password} = req.body;
     var type="User";
-    password= await bcrypt.hash(password,10); //figure out how to hash a password and save that hash to compare later.
-    var finduser=await users.find({username:username}).catch((err)=>console.log(err));
-    var findphonenumber=await users.find({phonenumber:phonenumber}).catch((err)=>console.log(err));
-    var findemail=await users.find({email:email}).catch((err)=>console.log(err));
-    if (finduser.length>0 || findphonenumber.length>0 || findemail.length>0)
-    {
-        res.status(400).json({message:"Account Already Exists"});
-    }
-    else{
-        const User=new users({username,type,email,phonenumber,password});
-        await User.save().then(()=>res.status(200).json({message:"Successful Register"})).catch((err)=>res.status(400).json({message:err}));
-        //the anonymous function inside .then promise handler would have a parameter that is related to the outer function which is User.save
-        //which is the response from User.save() basically.
+    try{
+        console.log("hi");
+        password= await bcrypt.hash(password,10);
+        console.log("hi");
+        var finduser=await users.find({username:username}).catch((err)=>console.log(err));
+        var findphonenumber=await users.find({phonenumber:phonenumber}).catch((err)=>console.log(err));
+        var findemail=await users.find({email:email}).catch((err)=>console.log(err));
+        if (finduser.length>0 || findphonenumber.length>0 || findemail.length>0)
+        {
+            res.status(400).json({message:"Account Already Exists"});
+        }
+        else{
+            console.log("fheahfahfah");
+            const User=new users({username,type,email,phonenumber,password});
+            await User.save().then(()=>res.status(200).json({message:"Successful Register"})).catch((err)=>res.status(400).json({message:err}));
+            //the anonymous function inside .then promise handler would have a parameter that is related to the outer function which is User.save
+            //which is the response from User.save() basically.
+        }
+    }catch(err){
+        console.log("fheahfahfah");
+        res.status(400).json({message:"bad"});
     }
 });
 
 router.put("/checkpassword",async(req,res,next)=>{
     var {username,currentpassword,newpassword}=req.body;
-    var query= await users.find({username:username}).catch((err)=>console.log(err));
-    var hashedPassword= await bcrypt.hash(newpassword,10);
-    if (query.length>0)
-    {
-        var valid= await bcrypt.compare(currentpassword,query[0].password);
-        var doesExist=await bcrypt.compare(newpassword,query[0].password);
-        if (valid)
+    try{
+        var query= await users.find({username:username}).catch((err)=>console.log(err));
+        var hashedPassword= await bcrypt.hash(newpassword,10);
+        if (query.length>0)
         {
-            if (!doesExist)
+            var valid= await bcrypt.compare(currentpassword,query[0].password);
+            var doesExist=await bcrypt.compare(newpassword,query[0].password);
+            if (valid)
             {
-                await users.updateOne({username:username},{password:hashedPassword});
-                res.status(200).json({message:"Password Successfully Changed"});
+                if (!doesExist)
+                {
+                    await users.updateOne({username:username},{password:hashedPassword});
+                    res.status(200).json({message:"Password Successfully Changed"});
+                }
+                else{
+                    res.status(400).json({message:"Current & New Password Matching"});
+                }
             }
             else{
-                res.status(400).json({message:"Current & New Password Matching"});
+                res.status(400).json({message:"Incorrect Password"});
             }
         }
         else{
-            res.status(400).json({message:"Incorrect Password"});
+            res.status(400);
         }
-    }
-    else{
-        res.status(400);
+    }catch(err){
+        res.status(400).json({message:err});
     }
 });
 
 router.put("/deleteaccount",async(req,res,next)=>{
     var {username}=req.body;
-    var query=await users.deleteOne({username:username});
-    if (query)
-    {
-        res.status(200).json({message:"Successfully Deleted Account"});  
-    }
-    else{
-        res.status(400).json({message:"Successfully Deleted Account"});
+    try{
+        var query=await users.deleteOne({username:username});
+        if (query)
+        {
+            res.status(200).json({message:"Successfully Deleted Account"});  
+        }
+        else{
+            res.status(400).json({message:"Successfully Deleted Account"});
+        }
+    }catch(err){
+        res.status(400).json({message:err});
     }
 })
 
