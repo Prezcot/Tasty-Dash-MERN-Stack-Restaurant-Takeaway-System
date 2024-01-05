3fa245144a63890fd0b00f59ed272991397b58c4
thinal12 <114849355+thinal12@users.noreply.github.com>
2024-01-05 16:42:09 +0530

    Created tests for home menu

---
+++ client/src/components/Tests/HomeMenu.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, fireEvent, waitFor,getAllByText,getByText } from "@testing-library/react";
+import Menu from "../Menu/Menu";
+import Item from "../Menu/Item";
+import Cart from "../Menu/Cart";
+import Basket from "../Order/Basket";
+import HomeMenu from "../Home/HomeMenu";
+import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+import '@testing-library/jest-dom';
+
+jest.mock("axios");
+window.setImmediate = window.setTimeout;
+
+describe("UNIT TEST - HOMEMENU COMPONENT", () => {
+    it("Menu Component Is Successfully Rendering", () => {
+        var {getAllByText} =render(
+            <BrowserRouter>
+                <HomeMenu/>
+            </BrowserRouter>);
+        const elementswithmenu=getAllByText("Our Offerings");
+        expect(elementswithmenu.length).toBeGreaterThan(0);
+    });
+
+    it("Menu Items Are Successfully Rendering", () => {
+        var {getAllByText} =render(
+            <BrowserRouter>
+                <HomeMenu/>
+            </BrowserRouter>);
+        setTimeout(()=>{
+            const elementswithmenu=getAllByText("$");// we use + because all items consist of that constant display and now the test would work regardless of which item is removed(unless its out of stock).
+            expect(elementswithmenu.length).toBeGreaterThan(0);
+        },5000);
+        
+    });
+});
