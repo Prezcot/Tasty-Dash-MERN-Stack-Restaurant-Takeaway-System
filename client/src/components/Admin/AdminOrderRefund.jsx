8e7e0edfcee12999ef66a159f2249dd050b6b230
Janindu Mapa <janindumapa100@gmail.com>
2024-01-06 14:20:55 +0530

    added search for the remaining pages

--- client/src/components/Admin/AdminOrderRefund.jsx
+++ client/src/components/Admin/AdminOrderRefund.jsx
@@ -8,6 +8,8 @@ import axios from "axios";
 
 const AdminOrderRefund = () => {
   const [data, setData] = useState([]);
+  const [search_query, setSearchQuery] = useState("");
+  const [filtered_order_data, setFilteredOrderData] = useState([]);
 
   var grabData = async () => {
     try {
@@ -24,28 +26,53 @@ const AdminOrderRefund = () => {
     grabData();
   }, []);
 
+  useEffect(() => {
+    setFilteredOrderData(
+      data.filter(
+        (item) =>
+          item.username.toLowerCase().includes(search_query.toLowerCase()) ||
+          item.order_id.toString().includes(search_query) ||
+          item.email.toLowerCase().includes(search_query.toLowerCase())
+      )
+    );
+  }, [search_query, data]);
+
   return (
     <>
       <div
-        className="display-6 text-light"
+        className="display-6 text-light d-flex justify-content-between align-items-center"
         style={{
           paddingLeft: "9px",
-          paddingTop: "76px",
-          paddingBottom: "10px",
-          margin: "0px",
+          paddingTop: "70px",
+          paddingBottom: "5px",
           backgroundColor: "#666666",
         }}
       >
         Refunds Necessary
+        <span style={{ paddingTop: "5px", paddingRight: "5px" }}>
+          <input
+            value={search_query}
+            style={{
+              borderRadius: "10px",
+            }}
+            type="search"
+            placeholder="Search Order"
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+        </span>
       </div>
       <ul className="list-group">
-        {data.map((items, index) => (
+        {filtered_order_data.map((items, index) => (
           <div key={index}>
             <li
               data-testid="list-item-test"
               className="list-group-item fs-6 list-group-item-danger"
             >
               <b>Name: {items.username}</b>
+              <br />
+              <b>Order ID: {items.order_id}</b>
               <br />
               <b>Paypal Email: {items.paypal_email}</b>
               <br />
