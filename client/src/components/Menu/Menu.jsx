891ba8fc0a1586f850aa1dfa2e2392b4eb3e1bb8
Janindu Mapa <janindumapa100@gmail.com>
2024-01-06 18:06:50 +0530

    fixed variable error

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -8,22 +8,23 @@ import NavBar from "../NavBar";
 import Dashboard from "../User/Dashboard";
 import Basket from "../Order/Basket";
 import { io } from "socket.io-client";
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function Menu() {
   const nav = useNavigate();
-  if(!sessionStorage.getItem("menu_cart")){
-    sessionStorage.setItem("menu_cart","{}");
+  if (!sessionStorage.getItem("menu_cart")) {
+    sessionStorage.setItem("menu_cart", "{}");
   }
   const [items, setItems] = useState([]);
   const [has_toast_appeared, setHasToastAppeared] = useState(false);
-  const [quantity_map, setQuantityMap] = useState(JSON.parse(sessionStorage.getItem("menu_cart")));
+  const [quantity_map, setQuantityMap] = useState(
+    JSON.parse(sessionStorage.getItem("menu_cart"))
+  );
 
   var grabitems = async () => {
-    try{
+    try {
       axios
         .get("http://localhost:3001/menu/data")
         .then((response) => {
@@ -33,14 +34,13 @@ function Menu() {
         .catch((error) => {
           console.error(error);
         });
-      }catch{
-        console.log("error");
-      }
+    } catch {
+      console.log("error");
+    }
   };
-  
+
   useEffect(() => {
     grabitems();
-    
   }, []);
 
   useEffect(() => {
@@ -53,58 +53,58 @@ function Menu() {
     };
   }, []);
 
+  sessionStorage.setItem("menu_cart", JSON.stringify(quantity_map));
 
-
-
-sessionStorage.setItem("menu_cart", JSON.stringify(quantity_map));
-
-  
-const updateItems = (itemName, itemImage,action) => {
+  const updateItems = (itemName, itemImage, action) => {
     setQuantityMap((prev_map) => {
       const current_quantity = prev_map[itemName] || 0;
       const new_quantity =
-        action === "add" ? current_quantity + 1 : Math.max(currentQuantity - 1, 0);
+        action === "add"
+          ? current_quantity + 1
+          : Math.max(current_quantity - 1, 0);
 
-        const updated_map = new_quantity === 0
-        ? Object.fromEntries(Object.entries(prev_map).filter(([key]) => key !== itemName))
-        : { ...prev_map, [itemName]: new_quantity };
+      const updated_map =
+        new_quantity === 0
+          ? Object.fromEntries(
+              Object.entries(prev_map).filter(([key]) => key !== itemName)
+            )
+          : { ...prev_map, [itemName]: new_quantity };
       showBasket();
       return updated_map;
     });
   };
-  
-function showBasket(){
-  if (!has_toast_appeared) {
-    toast.info('View your basket here   ➜', {
-      className : 'toast-position',
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: false,
-      draggable: true,
-      progress: undefined,
-      theme: 'dark',
-    });
 
-    
-    setHasToastAppeared(true);
+  function showBasket() {
+    if (!has_toast_appeared) {
+      toast.info("View your basket here   ➜", {
+        className: "toast-position",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+
+      setHasToastAppeared(true);
+    }
   }
-  };
 
-return (
-  <>
-  <NavBar style={{postion:"fixed"}}></NavBar>
-  <div className="menu-everything" style={{
-    display: "flex",
-    flexDirection: "column",
-    paddingTop: '4%',
-    marginTop: '0%'
-  }}>
-  
- 
-  
-  <style>
-    {`
+  return (
+    <>
+      <NavBar style={{ postion: "fixed" }}></NavBar>
+      <div
+        className="menu-everything"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: "4%",
+          marginTop: "0%",
+        }}
+      >
+        <style>
+          {`
       body {
         margin: 0;
         padding: 0;
@@ -117,62 +117,79 @@ return (
         color: white;
       }
     `}
-  </style>
+        </style>
 
-  
-  
-  <div className="menu-header">
-    <h1 align="center">Menu</h1>
-  </div>
+        <div className="menu-header">
+          <h1 align="center">Menu</h1>
+        </div>
 
-  {/* Separate items based on their types */}
-  <h2>Starters</h2>
-  
-  <div className="menu-item-div">
-    
-    {items.filter(item => item.itemType === 'starter').map(item => (
-      <Item
-        key={item.itemName}
-        item={item}
-        quantity={quantity_map[item.itemName] || 0}
-        onAddToCart={() => updateItems(item.itemName, item.itemImage, "add")}
-        onRemoveFromCart={() => updateItems(item.itemName, item.itemImage, "remove")}
-      />
-    ))}
-  </div>
-  <h2>Main Courses</h2>
-  <br></br>
-  <div className="menu-item-div">
-    {items.filter(item => item.itemType === 'mainCourse').map(item => (
-      <Item
-        key={item.itemName}
-        item={item}
-        quantity={quantity_map[item.itemName] || 0}
-        onAddToCart={() => updateItems(item.itemName, item.itemImage, "add")}
-        onRemoveFromCart={() => updateItems(item.itemName, item.itemImage, "remove")}
-      />
-    ))}
-  </div>
-  <h2>Desserts</h2>
-  <br></br>
-  <div className="menu-item-div">
-    {items.filter(item => item.itemType === 'dessert').map(item => (
-      <Item
-        key={item.itemName}
-        item={item}
-        quantity={quantity_map[item.itemName] || 0}
-        onAddToCart={() => updateItems(item.itemName, item.itemImage, "add")}
-        onRemoveFromCart={() => updateItems(item.itemName, item.itemImage, "remove")}
-      />
-    ))}
-  </div>
-  
-  <Cart items={items} quantity_map={quantity_map}  data-testid="cart-icon"/>
-</div>
+        {/* Separate items based on their types */}
+        <h2>Starters</h2>
 
-</>
-  
+        <div className="menu-item-div">
+          {items
+            .filter((item) => item.itemType === "starter")
+            .map((item) => (
+              <Item
+                key={item.itemName}
+                item={item}
+                quantity={quantity_map[item.itemName] || 0}
+                onAddToCart={() =>
+                  updateItems(item.itemName, item.itemImage, "add")
+                }
+                onRemoveFromCart={() =>
+                  updateItems(item.itemName, item.itemImage, "remove")
+                }
+              />
+            ))}
+        </div>
+        <h2>Main Courses</h2>
+        <br></br>
+        <div className="menu-item-div">
+          {items
+            .filter((item) => item.itemType === "mainCourse")
+            .map((item) => (
+              <Item
+                key={item.itemName}
+                item={item}
+                quantity={quantity_map[item.itemName] || 0}
+                onAddToCart={() =>
+                  updateItems(item.itemName, item.itemImage, "add")
+                }
+                onRemoveFromCart={() =>
+                  updateItems(item.itemName, item.itemImage, "remove")
+                }
+              />
+            ))}
+        </div>
+        <h2>Desserts</h2>
+        <br></br>
+        <div className="menu-item-div">
+          {items
+            .filter((item) => item.itemType === "dessert")
+            .map((item) => (
+              <Item
+                key={item.itemName}
+                item={item}
+                quantity={quantity_map[item.itemName] || 0}
+                onAddToCart={() =>
+                  updateItems(item.itemName, item.itemImage, "add")
+                }
+                onRemoveFromCart={() =>
+                  updateItems(item.itemName, item.itemImage, "remove")
+                }
+              />
+            ))}
+        </div>
+
+        <Cart
+          items={items}
+          quantity_map={quantity_map}
+          data-testid="cart-icon"
+        />
+      </div>
+    </>
   );
-};  
+}
 
 export default Menu;
