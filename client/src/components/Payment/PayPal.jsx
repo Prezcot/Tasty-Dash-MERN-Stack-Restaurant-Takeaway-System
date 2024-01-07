63337f094ed7fb223624536d571e54b05cfe962e
BinaraPremathilake <bcpremathilake.tec@gmail.com>
2024-01-07 16:37:43 +0530

    Code Cleanup In Payment Component

--- client/src/components/Payment/PayPal.jsx
+++ client/src/components/Payment/PayPal.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from "react";
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,9 +8,8 @@ import { io } from "socket.io-client";
 function Paypal() {
   const paypal = useRef();
   const nav = useNavigate();
-  let [latest_order_id,setLOID] = useState(0);
-  
-  
+  let [latest_order_id, setLOID] = useState(0);
+
   async function notifySuccess() {
     toast.success("Your order has been placed", {
       position: "top-center",
@@ -24,7 +23,7 @@ function Paypal() {
       draggable: true,
       progress: undefined,
       theme: "dark",
-      });
+    });
   }
   async function notifyFailure() {
     toast.error("order failed", {
@@ -36,7 +35,7 @@ function Paypal() {
       draggable: true,
       progress: undefined,
       theme: "dark",
-      });
+    });
   }
   useEffect(() => {
     window.paypal
@@ -57,9 +56,11 @@ function Paypal() {
         },
         onApprove: async (data, actions) => {
           const order = await actions.order.capture();
-          let response = await axios.get("http://localhost:3001/orders/get_order_id");
-          setLOID(latest_order_id=response.data);
-          let temp = latest_order_id+1;
+          let response = await axios.get(
+            "http://localhost:3001/orders/get_order_id"
+          );
+          setLOID((latest_order_id = response.data));
+          let temp = latest_order_id + 1;
           let order_details = {
             username: sessionStorage.getItem("username"),
             order_id: temp,
@@ -71,37 +72,45 @@ function Paypal() {
             instructions: sessionStorage.getItem("customer_instruction"),
             order_total: sessionStorage.getItem("total"),
           };
-    
-        await axios.post("http://localhost:3001/orders/addorder", order_details);
-        
-        let document_id = '6589770129060833d3f653b1';/*this document does not change and is singular in the mongodb collection*/
-        await axios.put(`http://localhost:3001/orders/update_order_id/${document_id}`, {temp});
 
-        const socket = io("http://localhost:3001");
-        socket.emit("order_status_update", { username: order_details.username, status: order_details.order_status });
+          await axios.post(
+            "http://localhost:3001/orders/addorder",
+            order_details
+          );
 
-        sessionStorage.removeItem("menu_cart");
-        sessionStorage.removeItem("cart");
-        sessionStorage.removeItem("order_id");
-        sessionStorage.removeItem("total");
-        sessionStorage.removeItem("customer_instruction");
-        
-        notifySuccess();
-        
-        
+          let document_id =
+            "6589770129060833d3f653b1"; /*this document does not change and is singular in the mongodb collection*/
+          await axios.put(
+            `http://localhost:3001/orders/update_order_id/${document_id}`,
+            { temp }
+          );
+
+          const socket = io("http://localhost:3001");
+          socket.emit("order_status_update", {
+            username: order_details.username,
+            status: order_details.order_status,
+          });
+
+          sessionStorage.removeItem("menu_cart");
+          sessionStorage.removeItem("cart");
+          sessionStorage.removeItem("order_id");
+          sessionStorage.removeItem("total");
+          sessionStorage.removeItem("customer_instruction");
+
+          notifySuccess();
         },
-        onError: (err) => {console.log(err);
+        onError: (err) => {
+          console.log(err);
           notifyFailure();
         },
       })
       .render(paypal.current);
-      
   }, []);
 
   return (
     <div>
       <div ref={paypal}></div>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   );
 }
--- client/src/components/Payment/Payment.jsx
+++ client/src/components/Payment/Payment.jsx
@@ -3,19 +3,17 @@ import Paypal from "./PayPal";
 import "../App.css";
 import NavBar from "../NavBar";
 
-function Payment ({ renderPayPal = true }){
-
-let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
-let total_amount = sessionStorage.getItem('total');
-let customer_ins = sessionStorage.getItem('customer_instruction');
-
+function Payment({ renderPayPal = true }) {
+  let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+  let total_amount = sessionStorage.getItem("total");
+  let customer_ins = sessionStorage.getItem("customer_instruction");
 
   return (
-  <>
-  <div className="everything" style={{paddingTop: '4%'}}>
-  <NavBar></NavBar>
-    <style>
-    {`
+    <>
+      <div className="everything" style={{ paddingTop: "4%" }}>
+        <NavBar></NavBar>
+        <style>
+          {`
       body {
         margin: 0;
         padding: 0;
@@ -27,41 +25,72 @@ let customer_ins = sessionStorage.getItem('customer_instruction');
         color:white;
       }
     `}
-    </style>
-  <h1 style={{marginTop:"3%", marginLeft:"10%", color:"#FFF"}}>Confirm Details and Checkout</h1><br/>
-  
-  <div className="payment-detail-button-container" style={{marginBottom:"10%", height:"fit-content"}}>
+        </style>
+        <h1 style={{ marginTop: "3%", marginLeft: "10%", color: "#FFF" }}>
+          Confirm Details and Checkout
+        </h1>
+        <br />
 
-  <div style={{display:"flex", flexDirection:"column",justifyContent:"space-around", height:"fit-content",borderRight:"7px solid orange", padding:"3%", paddingLeft:"3%", paddingRight:"17%"}}>
+        <div
+          className="payment-detail-button-container"
+          style={{ marginBottom: "10%", height: "fit-content" }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+              height: "fit-content",
+              borderRight: "7px solid orange",
+              padding: "3%",
+              paddingLeft: "3%",
+              paddingRight: "17%",
+            }}
+          >
+            <div>
+              <h3>Items Ordered</h3>
+              {cart.map((item, index) => {
+                let [name, price, quantity] = item.split(",");
+                return (
+                  <h6
+                    data-testid="payment-items-test"
+                    style={{ fontWeight: "normal" }}
+                  >
+                    - {name} (x{quantity})
+                  </h6>
+                );
+              })}
+            </div>
+            <br />
 
-    <div>
-      <h3>Items Ordered</h3>
-      {cart.map((item, index) => {
-      let [name, price, quantity] = item.split(",");
-      return (
-      <h6 data-testid="payment-items-test" style={{ fontWeight: 'normal' }}>- {name} (x{quantity})</h6>
-      )})}
-    </div><br/>
+            <div>
+              <h3>Special Instructions</h3>
+              <h6
+                data-testid="payment-instruction-test"
+                style={{ fontWeight: "normal" }}
+              >
+                {" "}
+                "{customer_ins}"
+              </h6>
+            </div>
+            <br />
 
-    <div>
-      <h3>Special Instructions</h3>
-      <h6 data-testid="payment-instruction-test" style={{ fontWeight: 'normal' }}> "{customer_ins}"</h6>
-    </div><br/>
+            <div>
+              <h3>Total Amount</h3>
+              <h5
+                data-testid="payment-total-test"
+                style={{ fontWeight: "normal", color: "orange" }}
+              >
+                {" "}
+                ${total_amount}
+              </h5>
+            </div>
+          </div>
 
-    <div>
-      <h3>Total Amount</h3>
-      <h5 data-testid="payment-total-test" style={{ fontWeight: 'normal', color:"orange"}}> ${total_amount}</h5>
-    </div>
-
-  </div>
-
-
-  <div className="pay-button">
-  {renderPayPal && <Paypal />}
-  </div>  
-</div>
-</div>
-  </>
+          <div className="pay-button">{renderPayPal && <Paypal />}</div>
+        </div>
+      </div>
+    </>
   );
 }
 
